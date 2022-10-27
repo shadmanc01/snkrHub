8401b22408d34fd567965c343b35f8d0320fe578
farouk0197 <fkatakpaou@gmail.com>
2022-10-27 19:36:29 -0400

    authentication

--- controllers/authenticationControllers.js
+++ controllers/authenticationControllers.js
@@ -1,20 +1,44 @@
 const bcrypt = require("bcrypt");
-const athentificationModels = require("../models/authenticationModels.js")
-
+const authentificationModels = require("../models/authenticationModels.js")
+const pool = require("../db.js");
 const postRegister = async (req,res) => {
- const {userId,password} = req.body
+ const {username,password} = req.body
+ 
+
+
    try {
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(password, salt);
-        const register = await athentificationModels.registerInfo(userId,hashedPassword)
-        console.log(register,hashedPassword)
+        const register = await authentificationModels.registerInfo(username,hashedPassword)
+        console.log(register,hashedPassword);
         res.send(register);
    } catch {
     res.send();
    }
 }
 
+const checkLogin = async (req,res) => {
+    const {username,password} = req.body;
+    const nameExist = await pool.query("SELECT * FROM users WHERE username = $1",[username]).then(result=> {return result.rows[0]});
+    if (!nameExist){
+        res.status(400).send("cannot find user")
+    }
+    try {
+        if (await bcrypt.compare(password,nameExist.password)){
+            const wishlist = await  pool.query("SELECT * FROM user_wishlist WHERE user_id = $1",[nameExist.id])
+            const collection =  await pool.query("SELECT * FROM collections WHERE user_id = $1",[nameExist.id])
+            const wishlistData = wishlist.rows
+            const collectionData = collection.rows
+            res.send({wishlistData,collectionData,userid: nameExist.id})
+        }else {
+
+        }
+    }catch {
+        res.status(500).send()
+    }
+}
+
 module.exports = {
     postRegister,
-
+    checkLogin,
 }
