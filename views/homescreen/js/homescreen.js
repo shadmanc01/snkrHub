b6118ac9dc5f0fedee834ddbe285bd7dcffcfa51
Shadman <shadmanchowdhury01@gmail.com>
2022-10-29 17:45:15 -0400

    feat: button functionality is immaculate

--- views/homescreen/js/homescreen.js
+++ views/homescreen/js/homescreen.js
@@ -6,6 +6,8 @@ let welcomeMessage = document.getElementById("greetingMessage");
 let welcomeMessage2 = document.getElementById("greetingMessage2");
 const searchButton = document.getElementById("sneakername");
 const submitButton = document.getElementById("submitButton");
+let sneakerDiv = document.getElementById("sneaker");
+let buttonDiv = document.getElementById("sneakerButtons");
 
 navLocker.addEventListener("click", removeMessage);
 navWish.addEventListener("click", removeMessage);
@@ -53,11 +55,37 @@ submitButton.addEventListener("click", (event) => {
         fetch(`http://localhost:3001/${snkr}`, request)
         .then(file=> file.json())
         .then(data=> {
-          console.log(data);
+          const snkrName = data[0].shoeName;
+          const snkrPrice = `Current Market Value: `+'$'+data[0].lowestResellPrice.stockX;
+          const snkrImg = data[0].thumbnail;
+          let nameDisplay = document.createElement("header");
+          nameDisplay.innerText = snkrName;
+          let priceDisplay = document.createElement("h4");
+          priceDisplay.innerText = snkrPrice;
+          const imgDisplay = document.createElement("img");
+          imgDisplay.src = snkrImg;
+          imgDisplay.width = 200;
+          imgDisplay.style.border = "thick solid #1E2431";
+          sneakerDiv.style.textAlign = "center";
+          sneakerDiv.appendChild(nameDisplay);
+          nameDisplay.appendChild(priceDisplay);
+          sneakerDiv.appendChild(imgDisplay);
+          let snkrLocker = document.createElement("button");
+          let snkrWish = document.createElement("button");
+          snkrLocker.className = "btn btn-primary me-md-2";
+          snkrWish.className = "btn btn-primary";
+          snkrLocker.type = "button";
+          snkrWish.type = "button";
+          snkrLocker.innerText = "add to snkrLocker";
+          snkrWish.innerText = "add to snkrWish";
+          buttonDiv.appendChild(snkrLocker);
+          buttonDiv.appendChild(snkrWish);
+          snkrLocker.style.margin = "10px";
+          snkrWish.style.margin = "10px"
         });
         // add functionality 
       }
       catch(error){
-       alert("user not found")
+       alert("sneaker not found")
       }
 })
